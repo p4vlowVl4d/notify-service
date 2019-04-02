7061b7500fe103a7a8738f5429586e7755c13c51
Pavlov Vladim <p4vlow.vl4d@yandex.ru>
2019-04-02 22:09:53 +0300

    simple game

--- web/router.go
+++ web/router.go
@@ -9,20 +9,30 @@ func Router(w ResponseWriter, r *Request) {
 	handle := indexHandle{
 		name: "index",
 	}
-	handle.Run(w, r.Header)
+	params := make(map[string]string)
+	params["name"] = "index"
+	data := RequestData{
+		Method: "index",
+		Params: params,
+		Role:   "admin",
+	}
+	handle.Run(w, data)
 }
+
 type WebHandle interface {
-	Run(w ResponseWriter, r Header)
+	Run(w ResponseWriter, data RequestData)
 }
 
 type indexHandle struct {
 	name string
 }
 
-func (i indexHandle) Run(w ResponseWriter, r Header) {
-	fmt.Fprintf(w, "This is a %s for content-type: %s ", i.name, r.Get("content-type"))
+func (i indexHandle) Run(w ResponseWriter, data RequestData) {
+	fmt.Fprintf(w, "Method %s Role %s", data.Method, data.Role)
 }
 
 type RequestData struct {
-	Header  string
+	Method string
+	Params map[string]string
+	Role   string
 }
