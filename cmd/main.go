26f651d3e2c51c587c7f91a28e9648810d6b7e74
Pavlov Vladim <p4vlow.vl4d@yandex.ru>
2019-04-12 00:00:55 +0300

    build architecture

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"github.com/p4vlowVl4d/notify-service/web"
+	"flag"
+	_ "github.com/p4vlowVl4d/notify-service/web"
 	config "github.com/spf13/viper"
-	"log"
-	"net/http"
+	"math/rand"
+	"time"
 )
 
-const (
-	defaultConfig = "config/config.yml"
-	defaultPortListen = ":3000"
+var (
+	CONFIG_FILE string = "config/config.yml"
 )
 
-func main() {
-	config.SetConfigFile(defaultConfig)
-	if err := config.ReadInConfig(); err != nil {
-		log.Fatal(err)
-	}
-	port := config.GetString("server.port")
-	fmt.Printf("Starting web server, listen on port: %s", port)
-	http.ListenAndServe(port, web.requestHandle)
+func Init() {
+	flag.StringVar(&CONFIG_FILE, "c", CONFIG_FILE, "путь к файлу конфигурации")
 }
 
-type WebData struct {
+func main() {
+	flag.Parse()
+	config.SetConfigFile(CONFIG_FILE)
+	if err := config.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	webConf := createWebConfig()
+	Execute(webConf)
+}
 
+//noinspection ALL
+func createWebConfig() *WebConf {
+	readTime, writeTimeout := config.GetInt32("server.readTimeout"), config.GetInt32("server.writeTimeout")
+	if readTime <= int32(0) || writeTimeout <= int32(0) {
+		panic("Please set timeout option in config file!")
+	}
+	port, host := config.GetString("server.port"), config.GetString("server.host")
+	if port == "" || host == "" {
+		panic("Please set port option in config file!")
+	}
+	readTime = rand.Int31n(readTime)
+	writeTimeout = rand.Int31n(writeTimeout)
+	return &WebConf{
+		ReadTimeout:  time.Duration(readTime),
+		WriteTimeout: time.Duration(writeTimeout),
+		PortListen:   port,
+		Host:         host,
+	}
 }
--- web/api.go
+++ web/api.go
@@ -1,7 +1,31 @@
 package web
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/p4vlowVl4d/notify-service/web"
+	"log"
+	"net/http"
+	"time"
+)
 
-func requestHandle(w http.ResponseWriter, r *http.Request) {
+func Execute(c *WebConf) {
+	fmt.Println("Configure web server...")
+	r := mux.NewRouter()
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         port,
+		Handler:      r,
+	}
+	r.Get("/").Handler(web.ApiServe)
+	fmt.Printf("Starting web server, listen on port: %s", port)
+	log.Println(srv.ListenAndServe())
+}
 
+type WebConf struct {
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	PortListen   string
+	Host         string
 }
