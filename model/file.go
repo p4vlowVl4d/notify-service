1d55ee0f0720f2b96df910d365ee5ccd072f7acc
Pavlov Vladim <p4vlow.vl4d@yandex.ru>
2019-04-14 16:48:50 +0300

    12wa2

--- model/file.go
+++ model/file.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -29,4 +32,33 @@ type UserError struct {
 
 func (e UserError) Error() string {
 	return strings.Join(e.errors, "\n")
+}
+
+func (f UserFile) save() (string, error) {
+	if f.User.Role.Save != true {
+		e := UserError{
+			f.User.ID,
+			f.User.Role,
+			[]string{"not enough rights to save files"},
+		}
+		return "error", e
+	}
+	filename := f.Name + f.Type
+	if f.Path == "null" {
+		f.Path = f.User.GetUserPath()
+		file, err := os.Create(strings.Join([]string{f.Path, filename}, "/"))
+		if err != nil {
+			return "", err
+		}
+		n, err := file.Write(f.Content)
+		if err != nil {
+			return fmt.Sprintf("error %d: %s", n, err.Error()), err
+		}
+		return file.Name(), nil
+	}
+	err := ioutil.WriteFile(filename, f.Content, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	return filename, nil
 }
--- web/saveHandler.go
+++ web/saveHandler.go
@@ -1,43 +1,11 @@
 package web
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
-	"github.com/p4vlowVl4d/notify-service/model"
 )
 
 func methodSave(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (f UserFile) save() (string, error) {
-	if f.User.Role.Save != true {
-		e := UserError{
-			f.User.ID,
-			f.User.Role,
-			[]string{"not enough rights to save files"},
-		}
-		return "error", e
-	}
-	filename := f.Name + f.Type
-	if f.Path == "null" {
-		f.Path = f.User.GetUserPath()
-		file, err := os.Create(strings.Join([]string{f.Path, filename}, "/"))
-		if err != nil {
-			return "", err
-		}
-		n, err := file.Write(f.Content)
-		if err != nil {
-			return fmt.Sprintf("error %d: %s", n, err.Error()), err
-		}
-		return file.Name(), nil
-	}
-	err := ioutil.WriteFile(filename, f.Content, os.ModePerm)
-	if err != nil {
-		return "", err
-	}
-	return filename, nil
-}
+
